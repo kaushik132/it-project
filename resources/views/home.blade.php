b18c7825e6c75f0a3eeeebcf0c8a85bc0f0ef5c5
kaushik132 <kaushik.dey.ahit@gmail.com>
2024-08-28 11:41:08 +0530

    new update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,30 +64,56 @@
 <div class="container-fluid feature bg-light py-5">
     <div class="container py-5">
         <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
-            <h4 class="text-primary">Our Project</h4>
+            <h4 class="text-primary">Our Features</h4>
             <h1 class="display-4 mb-4">Lorem ipsum dolor sit amet.</h1>
             <p class="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci facilis cupiditate recusandae aperiam temporibus corporis itaque quis facere, numquam, ad culpa deserunt sint dolorem autem obcaecati, ipsam mollitia hic.
             </p>
         </div>
         <div class="row g-4">
-
-    
-<div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.2s">
-    <div class="feature-item p-4 pt-0">
-        <div class="feature-icon p-4 mb-4">
-            <img src="" class="img-fluid rounded-top w-100" alt="Services">
-        </div>
-        <h4 class="mb-4">title</h4>
-        <p class="mb-4">Short contant
-        </p>
-        <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
-    </div>
-</div>
-
-
-
-    
-           
+            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.2s">
+                <div class="feature-item p-4 pt-0">
+                    <div class="feature-icon p-4 mb-4">
+                        <i class="fas fa-tv fa-3x"></i>
+                    </div>
+                    <h4 class="mb-4">Lorem, ipsum.</h4>
+                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
+                    </p>
+                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.4s">
+                <div class="feature-item p-4 pt-0">
+                    <div class="feature-icon p-4 mb-4">
+                        <i class="fas fa-database fa-3x"></i>
+                    </div>
+                    <h4 class="mb-4">Lorem, ipsum.</h4>
+                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
+                    </p>
+                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.6s">
+                <div class="feature-item p-4 pt-0">
+                    <div class="feature-icon p-4 mb-4">
+                        <i class="fa fa-robot fa-3x"></i>
+                    </div>
+                    <h4 class="mb-4">Lorem, ipsum.</h4>
+                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
+                    </p>
+                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.8s">
+                <div class="feature-item p-4 pt-0">
+                    <div class="feature-icon p-4 mb-4">
+                        <i class="fa fa-network-wired fa-3x"></i>
+                    </div>
+                    <h4 class="mb-4">Lorem, ipsum.</h4>
+                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
+                    </p>
+                    <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
